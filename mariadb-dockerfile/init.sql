070ea040177097450d2b6c7ebe6b2eca53b07267
akiyatomohiro <thairwars@gmail.com>
2023-05-20 11:35:05 +0900

    dockerでlocalを作り、AWSで本番デプロイ

--- mariadb-dockerfile/init.sql
+++ mariadb-dockerfile/init.sql
@@ -1,1 +1,2 @@
-grant all privileges on simplememo.* TO 'dbuser'@'127.0.0.1' identified by 'simplememodbuser';
+-- grant all privileges on simplememo.* TO 'dbuser'@'127.0.0.1' identified by 'simplememodbuser';
+grant all privileges on simplememo.* TO 'dbuser'@'%' identified by 'simplememodbuser';
