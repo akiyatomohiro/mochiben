6294825645a492e450bf5a8d428933b7de0f71e3
akiyatomohiro <thairwars@gmail.com>
2023-05-20 14:18:26 +0900

    サービス名、DB名、DBユーザー名の変更

--- mariadb-dockerfile/init.sql
+++ mariadb-dockerfile/init.sql
@@ -1,2 +1,2 @@
 -- grant all privileges on simplememo.* TO 'dbuser'@'127.0.0.1' identified by 'simplememodbuser';
-grant all privileges on simplememo.* TO 'dbuser'@'%' identified by 'simplememodbuser';
+grant all privileges on mochiben.* TO 'dbuser'@'%' identified by 'mochibendbuser';
