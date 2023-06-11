657eb2e7274cb20c8ef523c8a26ff96a9532430f
akiyatomohiro <thairwars@gmail.com>
2023-06-11 15:05:18 +0900

    メモ作成API作成、DBの時刻修正

--- config/database.php
+++ config/database.php
@@ -89,6 +89,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'timezone' => 'Asia/Tokyo',
         ],
 
     ],
@@ -123,7 +124,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
